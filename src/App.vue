f18b7e64b654694ccf728842ddbc9ebaa817f12e
Luca Gregori <luca.gregori98@gmail.com>
2023-12-04 16:52:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {AppHeader},
+  data() {
+    return {
+
+    };
+  },
+}
+</script>
+<template>
+
+<AppHeader /> 
+
+<h1>ciao utente</h1>
+
+</template>
+<style lang="scss">
+@use"./style/general.scss";
+
+</style>
