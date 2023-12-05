58f46110bc6faf69da5364049e1261d3190290d3
Luca Gregori <luca.gregori98@gmail.com>
2023-12-05 21:15:57 +0100

    Add: Latest News

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -4,13 +4,14 @@ import AppLearningDrive from './AppLearningDrive.vue';
 import AppCourses from './AppCourses.vue';
 import AppInstructors from './AppInstructors.vue';
 import AppTestimonials from './AppTestimonials.vue';
+import AppLatestNews from './AppLatestNews.vue';
 
 
 export default {
     data() {
         return {};
     },
-    components: { AppFreeLesson, AppLearningDrive, AppCourses, AppInstructors,AppTestimonials }
+    components: { AppFreeLesson, AppLearningDrive, AppCourses, AppInstructors, AppTestimonials, AppLatestNews }
 }
 </script>
 <template>
@@ -19,6 +20,7 @@ export default {
 <AppCourses />
 <AppInstructors />
 <AppTestimonials />
+<AppLatestNews />
 
 </template>
 <style scoped lang="scss">
---
+++ src/components/AppLatestNews.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<template>
+<div class="big-container">
+    <div class="small-container">
+        <div class="card">
+            <h2>Latest News</h2>
+            <div class="box-left">
+                <p><strong>What Car to Start With?</strong></p>
+                <p>February 7th, 2019 /</p>
+                <span> 0 Comments</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+            <div class="box-right">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+<style scoped lang="scss">
+
+</style>
