2d3058b659d567c648e2edec4a319da050550cfc
Luca Gregori <luca.gregori98@gmail.com>
2023-12-05 13:33:01 +0100

    Add: AppInstructors

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -2,19 +2,21 @@
 import AppFreeLesson from './AppFreeLesson.vue';
 import AppLearningDrive from './AppLearningDrive.vue';
 import AppCourses from './AppCourses.vue';
+import AppInstructors from './AppInstructors.vue';
 
 
 export default {
     data() {
         return {};
     },
-    components: { AppFreeLesson, AppLearningDrive,AppCourses }
+    components: { AppFreeLesson, AppLearningDrive, AppCourses, AppInstructors }
 }
 </script>
 <template>
 <AppFreeLesson />
 <AppLearningDrive />
 <AppCourses />
+<AppInstructors />
 
 </template>
 <style scoped lang="scss">
--- src/components/AppCourses.vue
+++ src/components/AppCourses.vue
@@ -20,10 +20,34 @@ export default {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente minima cumque provident esse voluptatem error, at quibusdam illum recusandae rem placeat nostrum, perferendis laborum quisquam delectus laudantium. Aspernatur, eligendi pariatur.</p>
             <AppButton :buttonText="buttontext"/>
         </div>
+        <div class="row">
+            <div class="col">
+                <div class="card-car">
+                    <img src="../assets/img/courses-passplus-200x200.jpg" alt="">
+                    <h4>Pass Plus</h4>
+                    <button><a href=""></a>LEARN MORE</button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-car">
+                    <img src="../assets/img/course-intensive-200x200.jpg" alt="">
+                    <h4>Intensive Course</h4>
+                    <button><a href=""></a>LEARN MORE</button>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-car">
+                    <img src="../assets/img/courses-instructor-200x200.jpg" alt="">
+                    <h4>Instructors</h4>
+                    <button><a href=""></a>LEARN MORE</button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
 <style scoped lang="scss">
+@use "../style/partials/variables" as *;
 .big-container {
     background-image: url(../assets/img/promise-background.jpg);
     background-size: contain;
@@ -34,6 +58,8 @@ export default {
 .small-container {
     width: 80%;
     margin: 0 auto;
+    display: flex;
+    align-items:flex-end;
 
     .card {
         img {
@@ -46,14 +72,48 @@ export default {
         h3 {
             padding: 1rem;
         }
-
+        width: 40%;
         text-align: center;
         padding: 3rem;
-        width: 30%;
         border: none;
         border-radius: 5px;
         position: relative;
         top: -30px;
+        p {
+            color: $btn-grey;
+        }
+    }
+    .row {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: nowrap;
+        width:60%;
+        margin-left: 10%;
+        padding-bottom: 2rem;
+        
+        h4 {
+            padding-top: 2rem;
+            text-align: center;
+            color: $btn-grey;
+        }
+        .card-car  {
+                text-align: center;
+                
+            button {
+                text-decoration: none;
+                border: 2px solid $btn-grey;
+                color: $btn-grey;
+                border-radius: 1rem;
+                padding: 5px;
+                background: none;
+                font-size: .8rem;
+                width: 100px;
+            }
+            a {
+                text-decoration: none;
+            }
+        }
     }
 }
 </style>
